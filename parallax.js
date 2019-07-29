3e7f4e792a058f1016368445f12d077a0e191ac1
manoj <manoj kumar>
2019-07-29 10:27:31 +0530

    initial commit

---
+++ parallax.js
@@ -0,0 +1,79 @@
+var image = document.getElementsByClassName('r1');
+new simpleParallax(image, {
+    orientation: 'left',
+    overflow: 'true',
+    scale: '2',
+    delay: '4'
+});
+var image = document.getElementsByClassName('r2');
+new simpleParallax(image, {
+    orientation: 'left',
+    overflow: 'true',
+    scale: '2',
+    delay: '4'
+});
+var image = document.getElementsByClassName('i1');
+new simpleParallax(image, {
+    orientation: 'up',
+    overflow: 'true',
+    scale: '1.4',
+    delay: '4'
+});
+//end of section 1
+
+var image = document.getElementsByClassName('b1');
+new simpleParallax(image, {
+    orientation: 'up',
+    overflow: 'true',
+    scale: '1.2',
+    delay: '4'
+});
+
+var image = document.getElementsByClassName('i2');
+new simpleParallax(image, {
+    orientation: 'up-right',
+    overflow: 'true',
+    scale: '2',
+    delay: '4'
+});
+// end of section 2
+
+var image = document.getElementsByClassName('i3');
+new simpleParallax(image, {
+    orientation: 'up',
+    overflow: 'true',
+    scale: '1.5',
+    delay: '4'
+});
+
+var image = document.getElementsByClassName('i4');
+new simpleParallax(image, {
+    orientation: 'up',
+    overflow: 'true',
+    scale: '1.5',
+    delay: '2'
+});
+//end of section 3
+var image = document.getElementsByClassName('i5');
+new simpleParallax(image, {
+    orientation: 'up',
+    overflow: 'true',
+    scale: '1.5',
+    delay: '2'
+});
+//end of section 4
+var image = document.getElementsByClassName('b2');
+new simpleParallax(image, {
+    orientation: 'up',
+    overflow: 'true',
+    scale: '1.2',
+    delay: '2'
+});
+var image = document.getElementsByClassName('i6');
+new simpleParallax(image, {
+    orientation: 'up',
+    overflow: 'true',
+    scale: '1.2',
+    delay: '2'
+});
+//end of section 5
